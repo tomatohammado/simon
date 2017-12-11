96646d285ead31e6f2fe88f496c75b3d85eafc69
Hammad Malik <hmd.node@gmail.com>
2017-12-11 10:36:58 -0500

    Implement click event to .game-button's
    Whenever a .game-button is pressed, it will toggle a .selected class that will apply some
    style to the given button
    
    Works for now, but something tells me I'll need to refactor it when I'm trying to call the
    same selectButton function outside of the click event

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ WDI Project 1
 - [css attribute selectors](https://www.w3schools.com/css/css_attribute_selectors.asp)
 - [jQuery `$(document).ready()`](https://learn.jquery.com/using-jquery-core/document-ready/)
 - [difference between `.ready()` and `.onload()`](https://stackoverflow.com/a/3698214)
+- (jQuery `.toggleClass()`)[https://api.jquery.com/toggleclass/]
+- (jQuery `.click()`)[https://api.jquery.com/click/]
+- (difference between `$(this)` and `event.target`)[https://stackoverflow.com/a/21667010]
+- (jQuery setTimeout)[https://www.sitepoint.com/jquery-settimeout-function-examples/]
--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,19 @@
 const $ = window.jQuery
 
 $(document).ready(function () {
-  console.log('jQuery loaded')
+  $('.game-button').click(selectButton)
 })
+
+/* Functions */
+
+function selectButton () {
+  /* callback function for when a .game-button is clicked
+  // it will add, then remove, a .selected class to indicate a button has been pressed
+  // - I will use another function to handle whether the button is being presented to the the user as part of the input pattern, or if it is part of the users input
+  // - I think the function to remove the .selected class can be abstracted out. I need to read the documentation on .click() and possibly .on() to see what my options are
+  */
+
+  let target = $(this)
+  target.toggleClass('selected')
+  setTimeout(() => target.toggleClass('selected'), 500)
+}
